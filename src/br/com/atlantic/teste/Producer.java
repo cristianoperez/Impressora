02399a63d86a08c6f110a0dbe894c280157022d1
crstn <crstn@Cristiano>
2013-10-11 12:39:44 -0300

    Trocado arquivo texto por hardcode

--- src/br/com/atlantic/teste/Producer.java
+++ src/br/com/atlantic/teste/Producer.java
@@ -1,16 +1,11 @@
 package br.com.atlantic.teste;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Producer implements Runnable {
 		
-	private final String PRODUCER_PATH = "C:/producer.txt";
 	private Queue<PrintJob> queue;
 	private String producerName;
 	
@@ -23,21 +18,14 @@ public class Producer implements Runnable {
 	public void run() {
 		synchronized (queue) {
 			try {
-				Path path = Paths.get(PRODUCER_PATH);
-				List<String> readAllLines = Files.readAllLines(path, StandardCharsets.UTF_8);
+				List<PrintJob> jobs = getJobs();
 				Random random = new Random();
-				for (String string : readAllLines) {
-					if(string != null && !string.isEmpty()){
-						String[] split = string.split("/");
-						PrintJob printJob = new PrintJob(split[0], Integer.parseInt(split[1]));
-						Thread.sleep(random.nextInt(5) * 1000);
-						queue.addBack(printJob);
-						System.out.println("[" + getProducerName() + "] Produzindo arquivo: " + printJob.getJobName() + ", numero de paginas: " + printJob.getNumberOfPages());
-						queue.notify();
-					}
+				for (PrintJob job : jobs) {
+					Thread.sleep(random.nextInt(5) * 1000);
+					queue.addBack(job);
+					System.out.println("[" + getProducerName() + "] Produzindo arquivo: " + job.getJobName() + ", numero de paginas: " + job.getNumberOfPages());
+					queue.notify();
 				}
-			} catch (IOException e) {
-				e.printStackTrace();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -48,5 +36,22 @@ public class Producer implements Runnable {
 		return producerName;
 	}
 	
+	public List<PrintJob> getJobs(){
+	
+		List<PrintJob> jobs = new ArrayList<>();
+		
+		jobs.add(new PrintJob("proposta.txt", 3));
+		jobs.add(new PrintJob("notas.doc", 1));
+		jobs.add(new PrintJob("estrutura.xml", 2));
+		jobs.add(new PrintJob("especificacao.doc", 4));
+		jobs.add(new PrintJob("relatorio.doc", 5));
+		jobs.add(new PrintJob("anotacoes.txt", 7));
+		jobs.add(new PrintJob("users.json", 4));
+		jobs.add(new PrintJob("producao.txt", 9));
+		jobs.add(new PrintJob("tabelas.doc", 1));
+		jobs.add(new PrintJob("lembretes.txt", 3));
+		
+		return jobs;
+	}
 	
 }
